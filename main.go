903019399f19ccb1770a9671e2c6f83e5dafd8a2
Ariq Fikri Tamamy <ariqrick@gmail.com>
2025-02-03 00:14:09 +0700

    Basic Authorization Middleware

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"encoding/base64"
 	"final-project-go-sanber/controllers"
 	"final-project-go-sanber/database"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -17,6 +19,52 @@ var (
 	err error
 )
 
+func Authentication(username, password string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(401, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		if !strings.HasPrefix(authHeader, "Basic ") {
+			c.JSON(401, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		auth := strings.TrimPrefix(authHeader, "Basic ")
+		decodedAuth, err := base64.StdEncoding.DecodeString(auth)
+		if err != nil {
+			c.JSON(401, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		decodedAuthStr := string(decodedAuth)
+		creds := strings.Split(decodedAuthStr, ":")
+		if len(creds) != 2 {
+			c.JSON(401, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		if creds[0] != username || creds[1] != password {
+			c.JSON(401, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		// if authHeader != fmt.Sprintf("Basic %s", username+":"+password) {
+		// 	c.JSON(401, gin.H{"error": "Unauthorized"})
+		// 	c.Abort()
+		// 	return
+		// }
+		c.Next()
+	}
+}
+
 func main() {
 	err = godotenv.Load("config/.env")
 
@@ -24,6 +72,7 @@ func main() {
 		panic("Error loading .env file")
 	}
 	psqlInfo := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`, os.Getenv("PGHOST"), os.Getenv("PGPORT"), os.Getenv("PGUSER"), os.Getenv("PGPASSWORD"), os.Getenv("PGDATABASE"))
+	// psqlInfo := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`, os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 
 	DB, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
@@ -40,11 +89,15 @@ func main() {
 	database.DBMigrate(DB)
 
 	router := gin.Default()
+	username := "admin"
+	password := "admin123"
+	router.Use(Authentication(username, password))
 	router.GET("/anime", controllers.GetAllAnime)
 	router.POST("/anime", controllers.InsertAnime)
 	router.PUT("/anime:id", controllers.UpdateAnime)
 	router.DELETE("/anime:id", controllers.DeleteAnime)
 
+	// router.Run(":8080")
 	router.Run(":" + os.Getenv("PGPORT"))
 	fmt.Println("Successfully Connected")
 }
